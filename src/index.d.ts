585dd36ce7faaafbcabb25938e2152765ea10fa3
Michael O'Brien <mob@sensedeep.com>
2021-04-13 15:47:31 +1000

    DEV: export Dynamo from index for V3 SDK users

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,8 @@
     dynamodb-onetable - DynamoDB wrapper for single table patterns
 */
 
+import Dynamo from './Dynamo'
 import { AnyEntity, AnyModel, Entity, Model, OneParams, OneProperties } from './Model'
 import { Table } from './Table'
 
-export { AnyEntity, AnyModel, Entity, Model, OneParams, OneProperties, Table }
+export { AnyEntity, AnyModel, Dynamo, Entity, Model, OneParams, OneProperties, Table }
