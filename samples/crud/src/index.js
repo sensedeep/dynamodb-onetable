1d98dc0191311cb9cf56dddf6cf28091f6e033dc
Michael O'Brien <mob@sensedeep.com>
2021-05-31 08:03:23 +1000

    DEV: update crud sample with create/delete table

--- samples/crud/src/index.js
+++ samples/crud/src/index.js
@@ -3,9 +3,14 @@
  */
 
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { Table } from 'dynamodb-onetable'
+
+//  For AWS V3
 import Dynamo from 'dynamodb-onetable/Dynamo'
-// import { Table } from 'dynamodb-onetable'
-import { Table } from '../../../dist/mjs/index.js'
+
+//  To debug locally
+//  import { Table } from '../../../dist/mjs/index.js'
+//  import Dynamo from '../../../dist/mjs/Dynamo.js'
 
 const client = new Dynamo({
     client: new DynamoDBClient({
@@ -13,11 +18,15 @@ const client = new Dynamo({
     })
 })
 
+/*
+    Single-table schema and setup. This is used for general access and by `createTable`
+ */
 const table = new Table({
     name: 'TestCrud',
     client: client,
     uuid: 'ulid',
     delimiter: '#',
+    logger: true,
     schema: {
         indexes: {
             primary: { hash: 'pk', sort: 'sk' },
@@ -34,31 +43,38 @@ const table = new Table({
             }
         }
     },
-    logger: (type, message, context) => {
-        if (type == 'trace' || type == 'data') return
-        console.log(type, message, JSON.stringify(context, null, 4))
-    }
 })
 
+
+//  Create a model to manage User entities
 const User = table.getModel('User')
 
 async function main() {
+
+    if (!(await table.exists())) {
+        await table.createTable()
+    }
+
+    //  Note: because the ID is auto-generated, this will create a new user each time.
+    //  Alternatively, set your own ID and use the 'exists: null' to allow updating.
     let user = await User.create({
         name: 'Peter Smith',
-    }, { log: true  })                  //  Emit console trace
-    console.log('CREATE', user)
+    }, { log: true  })                  //  Emit console trace for the command and result
+    console.log('CREATED user', user)
 
     user = await User.update({id: user.id, status: 'inactive'})
-    console.log('UPDATED', user)
+    console.log('UPDATED user', user)
 
     //  Scan is not normally advised -- scans entire table
     let users = await User.scan({})
-
-    console.log('FIND', users)
+    console.log('FOUND users', users)
 
     for (let user of users) {
         await User.remove({id: user.id})
     }
+
+    //  Cleanup. The argument is a safety string
+    await table.deleteTable('DeleteTableForever')
 }
 
 main()
