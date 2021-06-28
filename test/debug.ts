7b2bb67449ce6fd8bdcbdd6a0418d9c7ea6bf6ff
Michael O'Brien <mob@sensedeep.com>
2021-06-28 20:32:10 +1000

    DEV: add debug unit test

--- test/debug.ts
+++ test/debug.ts
@@ -4,38 +4,41 @@
 import {AWS, Client, Table, print, dump, delay} from './utils/init'
 import {DebugSchema} from './schemas'
 
-describe.skip('Debug only', () => {
-    const table = new Table({
-        name: 'DebugTest',
-        client: Client,
-        schema: DebugSchema,
-    })
-    const User = table.getModel('User')
+describe('Debug only', () => {
+const table = new Table({
+    name: 'DebugTest',
+    client: Client,
+    schema: DebugSchema,
+})
+const User = table.getModel('User')
 
-    test('Create', async() => {
-        if (!(await table.exists())) {
-            await table.createTable()
+test('Create', async() => {
+    if (!(await table.exists())) {
+        await table.createTable()
+    }
+})
+
+let user: any
+
+test('User Create', async() => {
+    user = await User.create({
+        name: 'Peter Smith',
+        location: {
+            zip: 98011,
+            address: '444 Cherry Tree Lane',
+            city: 'Seattle',
         }
     })
+})
 
-    let user: any
-
-    test('User Create', async() => {
-        user = await User.create({
-            name: 'Peter Smith',
-            location: {
-                zip: 98011,
-                address: '444 Cherry Tree Lane',
-                city: 'Seattle',
-            }
-        }, { log: true })                  //  Emit console trace for the command and result
+test('GetItem', async() => {
+    let item = await table.getItem({
+        pk: `user#${user.id}`,
+        sk: 'user#',
     })
+})
 
-    test('Remove User', async() => {
-        user = await User.update({id: user.id}, { set: { 'location.zip': '{98030}' } })
-    })
-
-    test('Destroy', async() => {
-        await table.deleteTable('DeleteTableForever')
-    })
+test('Destroy', async() => {
+    await table.deleteTable('DeleteTableForever')
+})
 })
