d650af8708582f841232e57eef9e478b4159a25d
Michael O'Brien <mob@sensedeep.com>
2021-06-29 14:58:28 +1000

    TEST: add table high level API test.

---
+++ test/table-high-level.ts
@@ -0,0 +1,147 @@
+/*
+    table-high-level.ts - Test CRUD on table high level API
+ */
+import {AWS, Client, Match, Table, print, dump, delay} from './utils/init'
+import {DefaultSchema} from './schemas'
+
+const table = new Table({
+    name: 'TableHighLevelTestTable',
+    client: Client,
+    schema: DefaultSchema,
+})
+
+let user: any
+let users: any[]
+
+test('Create Table', async() => {
+    if (!(await table.exists())) {
+        await table.createTable()
+        expect(await table.exists()).toBe(true)
+    }
+})
+
+test('Create', async() => {
+    user = await table.create('User', {name: 'Peter Smith', status: 'active'})
+    expect(user).toMatchObject({
+        name: 'Peter Smith',
+        status: 'active',
+    })
+})
+
+test('Get', async() => {
+    user = await table.get('User', {id: user.id})
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Peter Smith',
+        status: 'active',
+    })
+    expect(user.created).toEqual(expect.any(Date))
+    expect(user.updated).toEqual(expect.any(Date))
+    expect(user.id).toMatch(Match.uuid)
+})
+
+test('Get including hidden', async() => {
+    user = await table.get('User', {id: user.id}, {hidden: true})
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Peter Smith',
+        status: 'active',
+        sk: 'user#',
+        gs1pk: 'user#Peter Smith',
+        gs1sk: 'user#',
+    })
+    expect(user.created).toEqual(expect.any(Date))
+    expect(user.updated).toEqual(expect.any(Date))
+    expect(user.id).toMatch(Match.uuid)
+    expect(user.pk).toMatch(/^user#/)
+})
+
+test('Find by ID', async() => {
+    users = await table.find('User', {id: user.id})
+    expect(users.length).toBe(1)
+    user = users[0]
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Peter Smith',
+        status: 'active',
+    })
+})
+
+test('Find by name on GSI', async() => {
+    users = await table.find('User', {name: user.name}, {index: 'gs1'})
+    expect(users.length).toBe(1)
+    user = users[0]
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Peter Smith',
+        status: 'active',
+    })
+})
+
+test('Update', async() => {
+    user = await table.update('User', {id: user.id, status: 'inactive'})
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Peter Smith',
+        status: 'inactive',
+    })
+    expect(user.created).toEqual(expect.any(Date))
+    expect(user.updated).toEqual(expect.any(Date))
+    expect(user.id).toMatch(Match.uuid)
+})
+
+test('Remove attribute', async() => {
+    //  Remove attribute by setting to null
+    user = await table.update('User', {id: user.id, status: null})
+    expect(user.status).toBeUndefined()
+})
+
+test('Remove attribute 2', async() => {
+    //  Update and remove attributes using {remove}
+    user = await table.update('User', {id: user.id, status: 'active'}, {remove: ['gs1pk', 'gs1sk'], hidden: true})
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Peter Smith',
+        status: 'active',
+        sk: 'user#',
+    })
+    expect(user.gs1pk).toBeUndefined()
+    expect(user.gs1sk).toBeUndefined()
+    expect(user.created).toEqual(expect.any(Date))
+    expect(user.updated).toEqual(expect.any(Date))
+    expect(user.id).toMatch(Match.uuid)
+})
+
+test('Remove item', async() => {
+    await table.remove('User', {id: user.id})
+    user = await table.get('User', {id: user.id})
+    expect(user).toBeUndefined()
+})
+
+test('Scan', async() => {
+    user = await table.create('User', {name: 'Sky Blue', status: 'active'})
+    users = await table.scan('User', {})
+    expect(users.length).toBe(1)
+    user = users[0]
+    expect(user).toMatchObject({
+        _type: 'User',
+        name: 'Sky Blue',
+        status: 'active',
+    })
+})
+
+test('Remove all users', async() => {
+    users = await table.scan('User', {})
+    expect(users.length).toBe(1)
+
+    for (let user of users) {
+        await table.remove('User', {id: user.id})
+    }
+    users = await table.scan('User', {})
+    expect(users.length).toBe(0)
+})
+
+test('Destroy Table', async() => {
+    await table.deleteTable('DeleteTableForever')
+    expect(await table.exists()).toBe(false)
+})
