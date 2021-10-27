4fa74e82454c2e30320f7496aa0a8ebf068eba27
Michael O'Brien <mob@sensedeep.com>
2021-10-28 08:14:33 +1100

    DEV: update debug.ts test file

--- test/debug.ts
+++ test/debug.ts
@@ -1,20 +1,27 @@
 /*
     debug.ts - Just for debug
+
+    Edit your test case here and invoke via: "jest debug"
+
+    Or run VS Code in the top level directory and just run.
  */
 import {AWS, Client, Entity, Match, Model, Table, print, dump, delay} from './utils/init'
+
+//  Use this schema or declare your own inline here
 import {DebugSchema} from './schemas'
 
 jest.setTimeout(7200 * 1000)
 
+//  Change your table params as required
 const table = new Table({
     name: 'DebugTable',
     client: Client,
+    //  Change this if you are using an inline schema
     schema: DebugSchema,
-    uuid: 'ulid',
     logger: true,
 })
-const accountId = table.uuid()
 
+//  This will create a local table
 test('Create Table', async() => {
     if (!(await table.exists())) {
         await table.createTable()
@@ -22,37 +29,10 @@ test('Create Table', async() => {
     }
 })
 
-type UserType = Entity<typeof DebugSchema.models.User>
-let User = table.getModel<UserType>('User')
-let user: UserType = null
-let users: any
-
-class AdminUserModel extends Model<UserType> {
-    constructor() {
-        super(table, 'User')
-    }
-}
-const AdminUser = new AdminUserModel()
-
 test('Test', async() => {
-    user = undefined
-
-    user = await User.create({
-        email: 'coy@acme.com',
-        domain: 'local',
-    }, {log: false, hidden: true})
-
-    user = await AdminUser.create({
-        email: 'rr@acme.com',
-        domain: 'local',
-    }, {log: false, hidden: true})
-    // dump("USER", user)
-
-    // users = await User.find({sk: null}, {log: false, hidden: true})
-
-    // users = await AdminUser.find({}, {index: 'gs1', log: false, hidden: true})
-
-    // dump("USERS", users)
+    /*
+        Put your code here
+    */
 })
 
 test('Destroy Table', async() => {
