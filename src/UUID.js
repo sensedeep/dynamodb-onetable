c949aa3c2fd591d6a1be29d6ec75c72284897fde
Michael O'Brien <mob@sensedeep.com>
2021-11-12 09:41:33 +1100

    FIX: UUID - make compliant

--- src/UUID.js
+++ src/UUID.js
@@ -4,7 +4,7 @@
     Consider ULIDs which are crypto sortable.
 */
 export default function UUID() {
-    return 'xxxxxxxxxxxxxxxxyxxxxxxxxxyxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
         return v.toString(16)
     })
