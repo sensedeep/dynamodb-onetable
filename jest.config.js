43213571ea20370958dcc2d111ce83f1133c36be
Michael O'Brien <mob@sensedeep.com>
2023-02-16 07:44:18 +1100

    DEV: temporarily disable stream unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,34 +2,29 @@
    Jest configuration
  */
 module.exports = {
-    "coveragePathIgnorePatterns": [
-        "node_modules",
-        "test/utils"
-    ],
-    coverageDirectory: "coverage",
+    coveragePathIgnorePatterns: ['node_modules', 'test/utils'],
+    coverageDirectory: 'coverage',
     globals: {
-      __DYNAMODB__: null,
+        __DYNAMODB__: null,
     },
-    roots: [
-        "<rootDir>/src",
-        "<rootDir>/test"
-    ],
+    roots: ['<rootDir>/src', '<rootDir>/test'],
     testMatch: [
-        "**/*.[jt]s",
-        "!**/src/*.[jt]s",
-        "!**/setup.[jt]s",
-        "!**/init.[jt]s",
-        "!**/teardown.[jt]s",
-        "!**/helpers.[jt]s",
-        "!**/schemas/*",
-        "!**/proto.ts"
+        '**/*.[jt]s',
+        '!**/src/*.[jt]s',
+        '!**/setup.[jt]s',
+        '!**/init.[jt]s',
+        '!**/teardown.[jt]s',
+        '!**/helpers.[jt]s',
+        '!**/schemas/*',
+        '!**/proto.ts',
+        '!**/stream.ts',
     ],
     globalSetup: '<rootDir>/test/utils/setup.ts',
     globalTeardown: '<rootDir>/test/utils/teardown.ts',
-    testEnvironment: "node",
+    testEnvironment: 'node',
     resetMocks: true,
     transform: {
-        '^.+\\.(js|ts)$': 'ts-jest'
+        '^.+\\.(js|ts)$': 'ts-jest',
     },
-    verbose: undefined
-};
+    verbose: undefined,
+}
