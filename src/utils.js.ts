1d57b958fe9436dd05a836e3f2ae991f2f637e9d
Alex Stansfield <alex.stansfield@gmail.com>
2023-03-16 17:47:15 +0700

    added new method that returns all values from a template value

--- src/utils.js.ts
+++ src/utils.js.ts
@@ -33,3 +33,48 @@ export const getValueFromTemplate = (template, value, field) => {
 
     return value.slice(startPlace, value.indexOf(endStr))
 }
+
+export const getValuesFromTemplate = (template, value) => {
+    const regex = /\$\{[a-zA-Z1-9_]+}/gm
+    const values = {}
+
+    const matches = template.match(regex)
+    if (matches === null) {
+        return values
+    }
+
+    const numMatches = matches.length
+
+    let remainingTemplate = template
+    let remainingValue = value
+
+    for (let placeholderIndex = 0; placeholderIndex < numMatches; placeholderIndex++) {
+        const placeholder = matches[placeholderIndex]
+        const field = placeholder.slice(2, -1)
+
+        let placeholderSection = remainingTemplate
+        const nextPlaceholder = matches[placeholderIndex + 1]
+        if (nextPlaceholder !== undefined) {
+            const nextIndex = placeholderSection.indexOf(nextPlaceholder)
+            placeholderSection = placeholderSection.slice(0, nextIndex)
+        }
+
+        let placeholderValue = remainingValue
+        const [startStr, endStr] = placeholderSection.split(placeholder)
+        const startPlace = remainingValue.indexOf(startStr) + startStr.length
+
+        if (startPlace > 0) {
+            placeholderValue = remainingValue.slice(startPlace)
+        }
+
+        if (endStr.length > 0) {
+            placeholderValue = placeholderValue.slice(0, placeholderValue.indexOf(endStr))
+        }
+        values[field] = placeholderValue
+
+        remainingTemplate = remainingTemplate.slice(startStr.length + placeholder.length)
+        remainingValue = remainingValue.slice(startPlace + placeholderValue.length)
+    }
+
+    return values
+}
--- test/get-value-from-template.ts
+++ test/get-value-from-template.ts
@@ -1,5 +1,4 @@
-import {getValueFromTemplate} from "../src/utils.js";
-
+import {getValueFromTemplate, getValuesFromTemplate} from '../src/utils.js'
 
 describe('getValueFromTemplate', () => {
     const fieldsAndValues = [
@@ -9,9 +8,16 @@ describe('getValueFromTemplate', () => {
         {field: 'batchId', expected: '6ebe3440-a11f-4bfb-9fed-6df83867723e'},
     ]
 
-    test.each(fieldsAndValues)('Get $field value from template', async ({field, expected}) => {
+    test.each(fieldsAndValues)('Get $field value from verbose template', async ({field, expected}) => {
         const template = 'Account#${accountId}:Company#${companyId}:Product#${productId}:Batch#${batchId}'
         const value = 'Account#f51929e3-ea3f-4693-8480-99584adc07d8:Company#621be599-a4ed-4664-b5c3-2c30a8fde47e:Product#a82735a9-c088-42eb-93ec-dc14995a258c:Batch#6ebe3440-a11f-4bfb-9fed-6df83867723e'
+
+        expect(getValueFromTemplate(template, value, field)).toEqual(expected)
+    })
+
+    test.each(fieldsAndValues)('Get $field value from simple template', async ({field, expected}) => {
+        const template = '${accountId}#${companyId}#${productId}#${batchId}'
+        const value = 'f51929e3-ea3f-4693-8480-99584adc07d8#621be599-a4ed-4664-b5c3-2c30a8fde47e#a82735a9-c088-42eb-93ec-dc14995a258c#6ebe3440-a11f-4bfb-9fed-6df83867723e'
 
         expect(getValueFromTemplate(template, value, field)).toEqual(expected)
     })
@@ -28,5 +34,42 @@ describe('getValueFromTemplate', () => {
         const value = 'Account#f51929e3-ea3f-4693-8480-99584adc07d8:Company#621be599-a4ed-4664-b5c3-2c30a8fde47e'
 
         expect(getValueFromTemplate(template, value, 'productId')).toBeUndefined()
+    })
+})
+
+
+describe('getValuesFromTemplate', () => {
+    const testCases = [
+        {
+            template: 'Account#${accountId}:Company#${companyId}:Product#${productId}:Batch#${batchId}',
+            value: 'Account#f51929e3-ea3f-4693-8480-99584adc07d8:Company#621be599-a4ed-4664-b5c3-2c30a8fde47e:Product#a82735a9-c088-42eb-93ec-dc14995a258c:Batch#6ebe3440-a11f-4bfb-9fed-6df83867723e',
+            expected: {
+                'accountId': 'f51929e3-ea3f-4693-8480-99584adc07d8',
+                'companyId': '621be599-a4ed-4664-b5c3-2c30a8fde47e',
+                'productId': 'a82735a9-c088-42eb-93ec-dc14995a258c',
+                'batchId': '6ebe3440-a11f-4bfb-9fed-6df83867723e',
+            }
+        },
+        {
+            template: '${accountId}#${companyId}#${productId}#${batchId}',
+            value: 'f51929e3-ea3f-4693-8480-99584adc07d8#621be599-a4ed-4664-b5c3-2c30a8fde47e#a82735a9-c088-42eb-93ec-dc14995a258c#6ebe3440-a11f-4bfb-9fed-6df83867723e',
+            expected: {
+                'accountId': 'f51929e3-ea3f-4693-8480-99584adc07d8',
+                'companyId': '621be599-a4ed-4664-b5c3-2c30a8fde47e',
+                'productId': 'a82735a9-c088-42eb-93ec-dc14995a258c',
+                'batchId': '6ebe3440-a11f-4bfb-9fed-6df83867723e',
+            }
+        },
+    ]
+
+    test.each(testCases)('Get values from template', async ({template, value, expected}) => {
+        expect(getValuesFromTemplate(template, value)).toEqual(expected)
+    })
+
+    test('Get no values when template has no placeholders', () => {
+        const template = 'Account#{accountId}:NoPlaceholder'
+        const value = 'Account#{accountId}:NoPlaceholder'
+
+        expect(getValuesFromTemplate(template, value)).toEqual({})
     })
 })
