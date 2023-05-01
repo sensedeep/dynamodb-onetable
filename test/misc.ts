31aec3da1b170e91854339d386e6227dda63a268
Michael O'Brien <mob@sensedeep.com>
2023-05-01 11:15:12 +1000

    FIX: unit test for removed Dynamo

--- test/misc.ts
+++ test/misc.ts
@@ -1,8 +1,9 @@
 /*
     misc.ts - Miscellaneous tests
  */
-import {AWS, Client, Dynamo, Match, Table, print, dump, delay} from './utils/init'
+import {AWS, Client, Match, Table, print, dump, delay} from './utils/init'
 import {DefaultSchema} from './schemas'
+import Dynamo from '../src/Dynamo.js'
 
 const table = new Table({
     name: 'MiscTestTable',
