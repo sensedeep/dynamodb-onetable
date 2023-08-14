ef7736b46b5cf7ae5516a28fe657615a64bc2ca5
Robbie Mackay <robbie@shipveho.com>
2023-08-14 16:38:07 +1200

    Setup typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
-    "parserOptions": {
-        "ecmaVersion": 2021,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
     },
-    "env": {
-        "es6": true,
-        "node": true,
-        "jest": true
+    env: {
+        es6: true,
+        node: true,
+        jest: true,
     },
-    "extends": "eslint:recommended",
-    "ignorePatterns": ["**/dist/"],
-    "rules": {
-        "linebreak-style": [ "error", "unix" ],
-        "quotes": [ "error", "single", { "allowTemplateLiterals": true } ],
-        "semi": [ "error", "never" ],
-        "no-case-declarations": "off",
-        "indent": [ "error", 4, { "SwitchCase": 0, "flatTernaryExpressions": true } ],
-        "object-curly-spacing": ["error", "never"]
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    ignorePatterns: ['**/dist/', '*.d.ts'],
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single', {allowTemplateLiterals: true}],
+        semi: ['error', 'never'],
+        'no-case-declarations': 'off',
+        indent: ['error', 4, {SwitchCase: 0, flatTernaryExpressions: true}],
+        'object-curly-spacing': ['error', 'never'],
     },
-    "globals": {
-        "expect": true,
-        "it": true
-    }
+    globals: {
+        expect: true,
+        it: true,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
 }
