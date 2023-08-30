8b6bf6ddbc920ec8cc75360c3f2f124a9e587a66
Michael O'Brien <mob@sensedeep.com>
2023-08-30 13:36:48 +1000

    DEV: update deps and tsconfig

--- test/metric.ts
+++ test/metric.ts
@@ -1,8 +1,7 @@
 /*
-    basic.ts - Basic create / get
+    metric.ts - Metrics test
  */
 import {AWS, Client, Entity, Match, Model, Table, print, dump, delay} from './utils/init'
-import {OneSchema} from '../src/index.js'
 
 jest.setTimeout(7200 * 1000)
 
@@ -20,20 +19,21 @@ const schema = {
     },
     models: {
         User: {
-            pk: { type: String, value: '${_type}#${email}' },
-            sk: { type: String, value: '${_type}#' },
-            email: { type: String, required: true },
-            balance: { type: Number, default: 0},
-        }
+            pk: {type: String, value: '${_type}#${email}'},
+            sk: {type: String, value: '${_type}#'},
+            email: {type: String, required: true},
+            balance: {type: Number, default: 0},
+        },
     } as const,
 }
 
 const table = new Table({
-    name: 'DebugTable',
+    name: 'MetricTable',
     client: Client,
     partial: true,
     schema,
     logger: true,
+    metrics: {custom: true, env: false},
 })
 
 //  This will create a local table
@@ -44,18 +44,21 @@ test('Create Table', async () => {
     }
 })
 
-test('Test', async () => {
+//  TODO - need many more tests
+test('Test Metrics', async () => {
     let User = table.getModel('User')
-    let user = await User.create({
-        email: "user@example.com",
-    })
+    let user = await User.create({email: 'user@example.com'})
     expect(user).toBeDefined()
     expect(user.email).toBe('user@example.com')
-    
-    user = await User.update({email: user.email}, {set: {balance: '${balance} + {10.55}'}})
 
-    let users = await User.find({email: user.email})
+    await table.flushMetrics()
+
+    user = await User.update({email: user.email}, {set: {balance: '${balance} + {10.55}'}})
+    await table.flushMetrics()
+
+    let users = await User.find({email: user.email}, {profile: 'find-user-by-email'})
     expect(users.length).toBe(1)
+    await table.flushMetrics()
 })
 
 test('Destroy Table', async () => {
