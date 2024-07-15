804df7a4991d742c633532b1cc49c4dec14206ef
Thomas Smeele <thomas.smeele@student.hu.nl>
2024-07-15 17:09:58 +0200

    Only base the type of the defined type in items when the type is an array type

--- src/Model.d.ts
+++ src/Model.d.ts
@@ -100,11 +100,13 @@ export type OneSchemaParams = {
 /*
     Entity field signature generated from the schema
  */
-type EntityField<T extends OneField> = T['items'] extends object
-  ? EntityField<T['items']>[]
-  : T['enum'] extends readonly EntityFieldFromType<T>[]
-    ? T['enum'][number]
-    : EntityFieldFromType<T>
+type EntityField<T extends OneField> = T['type'] extends 'array' | ArrayConstructor | ArrayBufferConstructor
+    ? T['items'] extends object
+        ? EntityField<T['items']>[]
+        : EntityFieldFromType<T>
+    : T['enum'] extends readonly EntityFieldFromType<T>[]
+        ? T['enum'][number]
+        : EntityFieldFromType<T>
 
 type EntityFieldFromType<T extends OneField> = T['type'] extends ArrayConstructor | 'array'
     ? ArrayItemType<T>[]
